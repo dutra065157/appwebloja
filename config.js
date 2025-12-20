d83a8a90f8a69c90d3473609942ac1c631cfbcff
dutra065157 <santosrenato8242@gmail.com>
2025-12-20 02:27:17 -0300

    atualizacao

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // --- CONFIGURAÇÃO GLOBAL DA API ---
 // Este é o ÚNICO lugar onde você precisa definir a URL do seu backend.
 // Para produção, altere para a URL fornecida pelo Render (ex: 'https://seu-app.onrender.com').
-const API_BASE_URL = "http://localhost:8000"; // URL do backend local
+const API_BASE_URL = "https://appwebloja-api.onrender.com"; // URL do backend local
